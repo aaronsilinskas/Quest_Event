b76a0f94d71edf6cb36adfc5b267208091fe5059
Aaron Silinskas <aaron@mindwidgets.com>
2019-01-21 13:10:34 -0800

    test: event without data

--- test/test_eventencoder/test_eventencoder.cpp
+++ test/test_eventencoder/test_eventencoder.cpp
@@ -24,13 +24,32 @@ void test_new_instance_is_reset_to_no_encoded_data()
     }
 }
 
+void test_event_without_data()
+{
+    Quest_EventEncoder ee = Quest_EventEncoder(buffer, BUFFER_SIZE);
+    Event e;
+    e.teamID = 2;   // 0b010
+    e.playerID = 4; // 0b0100
+    e.eventID = 21; // 0b10101
+    e.dataLengthInBits = 0;
+
+    bool result = ee.encodeToBuffer(&e);
+    TEST_ASSERT_EQUAL(EventEncoded, ee.encodeState);
+    TEST_ASSERT_EQUAL(true, result);
+    TEST_ASSERT_EQUAL(12, ee.encodedBitCount);
+    TEST_ASSERT_EQUAL(0b01000000, buffer[0] & 0b11100000); // team ID
+    TEST_ASSERT_EQUAL(0b00001000, buffer[0] & 0b00011110); // player ID
+    TEST_ASSERT_EQUAL(0b00000001, buffer[0] & 0b00000001); // event ID (1 bit)
+    TEST_ASSERT_EQUAL(0b01010000, buffer[1] & 0b11110000); // event ID (4 bits)
+}
+
 void test_event_with_data()
 {
     Quest_EventEncoder ee = Quest_EventEncoder(buffer, BUFFER_SIZE);
     Event e;
-    e.teamID = 1;
-    e.playerID = 2;
-    e.eventID = 3;
+    e.teamID = 1;   // 0b001
+    e.playerID = 2; // 0b0010
+    e.eventID = 3;  // 0b00011
     e.data[0] = 0b10101010;
     e.data[1] = 0b01000000;
     e.dataLengthInBits = 10;
@@ -47,7 +66,6 @@ void test_event_with_data()
     TEST_ASSERT_EQUAL(0b10100100, buffer[2] & 0b11111100); // data (6 bits)
 }
 
-// test event without data
 // test encode clears previous buffer
 // test team too large
 // test player too large
@@ -61,6 +79,7 @@ void setup()
     UNITY_BEGIN();
 
     RUN_TEST(test_new_instance_is_reset_to_no_encoded_data);
+    RUN_TEST(test_event_without_data);
     RUN_TEST(test_event_with_data);
 
     UNITY_END();
