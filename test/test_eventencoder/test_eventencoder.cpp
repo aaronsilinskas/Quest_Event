d4731b5d33a4d71e40ff5416d620bc1ceca38774
Aaron Silinskas <aaron@mindwidgets.com>
2019-01-21 13:34:31 -0800

    test: encode clears previous buffer

--- test/test_eventencoder/test_eventencoder.cpp
+++ test/test_eventencoder/test_eventencoder.cpp
@@ -66,11 +66,47 @@ void test_event_with_data()
     TEST_ASSERT_EQUAL(0b10100100, buffer[2] & 0b11111100); // data (6 bits)
 }
 
-// test encode clears previous buffer
+void test_encode_clears_previous_buffer()
+{
+    Quest_EventEncoder ee = Quest_EventEncoder(buffer, BUFFER_SIZE);
+    Event e;
+    e.teamID = 1;
+    e.playerID = 2;
+    e.eventID = 3;
+    // fill the event data with non-zero values
+    for (uint8_t i = 0; i < EVENT_DATA_LENGTH; i++)
+    {
+        e.data[i] = 0b01010101;
+    }
+    e.dataLengthInBits = EVENT_DATA_LENGTH * 8;
+
+    bool result = ee.encodeToBuffer(&e);
+    TEST_ASSERT_EQUAL(true, result);
+    // make sure the buffer has the event data
+    for (uint8_t i = 2; i < EVENT_DATA_LENGTH + 1; i++)
+    {
+        Serial.println(i);
+        TEST_ASSERT_NOT_EQUAL(0, buffer[i]);
+    }
+
+    // update the event to contain no data
+    e.eventID = 4; // 0b0100
+    e.dataLengthInBits = 0;
+    result = ee.encodeToBuffer(&e);
+    TEST_ASSERT_EQUAL(true, result);
+    TEST_ASSERT_EQUAL(0b01000000, buffer[1] & 0b11111111); // event ID (4 bits)
+
+    // the event data from the first event should be cleared out
+    for (uint8_t i = 2; i < EVENT_DATA_LENGTH + 1; i++)
+    {
+        TEST_ASSERT_EQUAL(0, buffer[i]);
+    }
+}
+
 // test team too large
 // test player too large
 // test event ID too large
-// test write buffer overflow
+// test write buffer overflow for each field and data
 
 void setup()
 {
@@ -81,6 +117,7 @@ void setup()
     RUN_TEST(test_new_instance_is_reset_to_no_encoded_data);
     RUN_TEST(test_event_without_data);
     RUN_TEST(test_event_with_data);
+    RUN_TEST(test_encode_clears_previous_buffer);
 
     UNITY_END();
 }
